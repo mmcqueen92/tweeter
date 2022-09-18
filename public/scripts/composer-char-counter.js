9c71aa429b7cfa2742667ced0073671a62ea8c2b
Malcolm McQueen <malcolmmcqueen@live.com>
2022-09-18 02:51:10 +0000

    cleaned css style sheets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,19 @@
-// $(document).ready(function() {
-//   // --- our code goes here ---
-//   console.log('doc ready')
-// });
-
-
 $(document).ready(function() {
   // --- our code goes here ---
+  // CREATE JQUERY VARIABLE FOR FORM INPUT
   const textBox = $('#tweet-text');
+  // FUNCTION TO RUN ON INPUT EVENT
   $(textBox).on("input", function (evt) {
+    // LENGTH OF CURRENT TEXT IN FORM INPUT
     let chars = $(this).val().length;
+    // REMAINING CHARACTER LIMIT
     let charLimit = 140 - chars;
+    // FIND COUNTER ELEMENT
     let counter = $(this).parent().next('div').children('.counter')
+    // UPDATE COUNTER TEXT TO CURRENT CHAR LIMIT
     counter.text(charLimit);
 
+    // RED TEXT IF CHARACTER LIMIT IS EXCEEDED
     if (charLimit < 0) {
       counter.addClass('text-color-red');
     } else if (charLimit >= 0) {
